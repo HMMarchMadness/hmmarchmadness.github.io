37e1a49e2390a69cd19473847fdbcf07be84f2f7
Pratham Gandhi <pratham_gandhi@horacemann.org>
2018-03-09 23:51:35 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,30 @@
+function pickWinner(winner,nextID){
+	console.log(winner,nextID)
+	var next = document.getElementById(nextID);
+	if(nextID.substring(1,3)=="1A"||nextID.substring(1,3)=="1B"){
+		var num = parseInt(nextID.substring(1,2));
+		num++;
+		var nextNextID = nextID.substring(0,1)+num.toString()+"AResult";
+	}
+	else if(nextID.substring(1,3)=="1C"||nextID.substring(1,3)=="1D"){
+		var num = parseInt(nextID.substring(1,2));
+		num++;
+		var nextNextID = nextID.substring(0,1)+num.toString()+"BResult";
+	}
+	else if(nextID.substring(1,3)=="2A"||nextID.substring(1,3)=="2B"){
+		var num = parseInt(nextID.substring(1,2));
+		num++;
+		var nextNextID = nextID.substring(0,1)+num.toString()+"AResult";
+	}
+	var nextButton = "<button onclick=\"pickWinner('"+winner+"','"+nextNextID+"'"+")\">"+winner+"</button>";
+	var HTMLToAppend = StringToHTML(nextButton);
+	next.appendChild(HTMLToAppend);
+	console.log(nextButton);
+}
+
+function StringToHTML(html) {
+    var template = document.createElement('template');
+    html = html.trim(); // Never return a text node of whitespace as the result
+    template.innerHTML = html;
+    return template.content;
+  }
